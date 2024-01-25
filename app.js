80ccd13d4368bfc64774b65e5d9127d509ecb270
AlexsanderDiego <alexdiegoat@gmail.com>
2024-01-24 23:47:20 -0300

    projetoAPI

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const server = express()
+
+const usuarios = [
+    {
+        email: 'test@example.com',
+        senha: '123'
+    },
+    {
+        email: 'usuario@example.com',
+        senha: '123'
+    },
+]
+
+server.use(express.json())
+
+server.post('/login', (req, res) => {
+
+    const usuarioEncontrado = usuarios.find((usuario) => usuario.email == req.body.email)
+
+    if (usuarioEncontrado != null && req.body.senha == usuarioEncontrado.senha) {
+        res.send('OK..Login Autorizado')
+    } else {
+        res.status(401).send('Não Autorizado')
+    }
+
+})
+
+server.listen(8080)
