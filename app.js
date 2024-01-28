91aeb10d19fc02227e4a842d164228d1d8a89dff
AlexsanderDiego <alexdiegoat@gmail.com>
2024-01-27 21:41:15 -0300

    atualizando back

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
-const express = require('express')
+// const express = require('express')
 
-const server = express()
+// const server = express()
+
+// const usuarios = [
+//     {
+//         email: 'test@example.com',
+//         senha: '123'
+//     },
+//     {
+//         email: 'usuario@example.com',
+//         senha: '123'
+//     },
+// ]
+
+// server.use(express.json())
+
+// server.post('/login', (req, res) => {
+
+//     const usuarioEncontrado = usuarios.find((usuario) => usuario.email == req.body.email)
+
+//     if (usuarioEncontrado != null && req.body.senha == usuarioEncontrado.senha) {
+//         res.send('OK..Login Autorizado')
+//     } else {
+//         res.status(401).send('Não Autorizado')
+//     }
+
+// })
+
+// server.listen(8080)
+
+const express = require('express');
+const cors = require('cors'); // Adicionando o módulo CORS
+
+const server = express();
 
 const usuarios = [
-    {
-        email: 'test@example.com',
-        senha: '123'
-    },
-    {
-        email: 'usuario@example.com',
-        senha: '123'
-    },
-]
+  {
+    email: 'test@example.com',
+    senha: '123'
+  },
+  {
+    email: 'usuario@example.com',
+    senha: '123'
+  },
+];
 
-server.use(express.json())
+server.use(express.json());
+
+// Middleware CORS
+server.use(cors());
 
 server.post('/login', (req, res) => {
+  const usuarioEncontrado = usuarios.find((usuario) => usuario.email == req.body.email);
 
-    const usuarioEncontrado = usuarios.find((usuario) => usuario.email == req.body.email)
+  if (usuarioEncontrado != null && req.body.senha == usuarioEncontrado.senha) {
+    res.send('OK..Login Autorizado');
+  } else {
+    res.status(401).send('Não Autorizado');
+  }
+});
 
-    if (usuarioEncontrado != null && req.body.senha == usuarioEncontrado.senha) {
-        res.send('OK..Login Autorizado')
-    } else {
-        res.status(401).send('Não Autorizado')
-    }
-
-})
-
-server.listen(8080)
+server.listen(8080, () => {
+  console.log('Servidor está rodando na porta 8080');
+});
