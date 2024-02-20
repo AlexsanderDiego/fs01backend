3eeab5c7dcee97980f8d308c47cc617f3236c0f3
AlexsanderDiego <alexdiegoat@gmail.com>
2024-02-19 22:32:50 -0300

    Adiciona importação do prisma e substitui consulta SQL por consulta do prisma

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import bcrypt from "bcrypt"; // Adicionando o encript de senha
-import sql from "../config/db.js";
+import sql, { prisma } from "../config/db.js";
 
 const authRoutes = Router();
 
@@ -8,8 +8,15 @@ const authRoutes = Router();
 authRoutes.post("/auth/login", async (req, res) => {
     const loginUsuario = req.body;
   
-    const resposta =
-      await sql`SELECT * FROM usuarios  where email = ${loginUsuario.email}`;
+    // const resposta =
+    //   await sql`SELECT * FROM usuarios  where email = ${loginUsuario.email}`;
+      const resposta =  await prisma.usuarios.findMany({
+        where: {
+          email: loginUsuario.email,
+        },
+      });
+      console.log(resposta);
+      console.log(resposta.length);
   
     if (
       resposta.length &&
